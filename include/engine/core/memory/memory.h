45c3fa9b0a21f513be055cdd52c16d5f6b40d852
Milos Zivlak <zivlakmilos@gmail.com>
2016-10-04 01:06:01 +0200

    Fix: Forget to include memory classes headers in memory.h

--- include/engine/core/memory/memory.h
+++ include/engine/core/memory/memory.h
@@ -4,4 +4,8 @@
 #include "engine/core/memory/baseresourcehandler.h"
 #include "engine/core/memory/basecresourcehandler.h"
 
+#include "engine/core/memory/arrayhandler.h"
+
+#include "engine/core/memory/cfilehandler.h"
+
 #endif // _ENGINE_CORE_MEMORY_MEMORY_H_
