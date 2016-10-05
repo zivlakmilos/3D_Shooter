fca4a1534d2b4a32e1c9227acd8fe3ee08c4e1fb
Milos Zivlak <zivlakmilos@gmail.com>
2016-10-06 01:37:54 +0200

    Added constants for axes

--- include/engine/core/constants.h
+++ include/engine/core/constants.h
@@ -1,9 +1,24 @@
 #ifndef _ENGINE_CORE_CONSTANTS_H_
 #define _ENGINE_CORE_CONSTANTS_H_
 
+#include <glm/glm.hpp>
+
 namespace zi
 {
     const double PI = 3.1415926535897932384;
+    
+    enum Axis {
+        AxisX = 0,
+        AxisY,
+        AxisZ
+    };
+    
+    const glm::vec3 right(1.0f, 0.0f, 0.0f);
+    const glm::vec3 left(-1.0f, 0.0f, 0.0f);
+    const glm::vec3 up(0.0f, 1.0f, 0.0f);
+    const glm::vec3 down(0.0f, -1.0f, 0.0f);
+    const glm::vec3 forward(0.0f, 0.0f, -1.0f);
+    const glm::vec3 back(0.0f, 0.0f, 1.0f);
 }
 
 #endif // _ENGINE_CONSTANTS_H_
